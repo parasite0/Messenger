2b0b7b5c05ba7606f1537d4921c3029432319ef0
parasite0 <37079401+parasite0@users.noreply.github.com>
2022-11-03 19:57:47 +0500

    Создание сервера и консольного клиента

--- ConsoleClient/Messenger/Program.cs
+++ ConsoleClient/Messenger/Program.cs
@@ -1,13 +1,48 @@
 ﻿using System;
+using Newtonsoft.Json;
 
 namespace Messenger
 {
     class Program
     {
+        private static int MessageID;
+        private static string UserName;
+        private static MessengerClientAPI API = new MessengerClientAPI();
+
+        private static void GetNewMessages()
+        {
+            Message msg = API.GetMessage(MessageID);
+            while (msg != null)
+            {
+                Console.WriteLine(msg);
+                MessageID++;
+                msg = API.GetMessage(MessageID);
+            }
+        }
         static void Main(string[] args)
         {
-            Message msg = new Message();
-            Console.WriteLine(msg.ToString());
+            //Message msg = new Message("Vlad", "Privet", DateTime.UtcNow);
+            //string output = JsonConvert.SerializeObject(msg);
+            //Console.WriteLine(output);
+            //Message deserializedMsg = JsonConvert.DeserializeObject<Message>(output);
+            //Console.WriteLine(deserializedMsg);
+            //{ "UserName":"Vlad","MessageText":"Privet","TimeStamp":"2022-11-03T13:42:17.9553251Z"}
+            //Vlad < 03.11.2022 13:42:17 >: Privet
+            MessageID = 1;
+            Console.WriteLine("Введите Ваше имя:");
+            //UserName = "Vlad";
+            UserName = Console.ReadLine();
+            string MessageText = "";
+            while (MessageText != "exit")
+            {
+                GetNewMessages();
+                MessageText = Console.ReadLine();
+                if (MessageText.Length > 1)
+                {
+                    Message Sendmsg = new Message(UserName, MessageText, DateTime.Now);
+                    API.SendMessage(Sendmsg);
+                }
+            }
         }
     }
 }

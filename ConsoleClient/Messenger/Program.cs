8df1ddbd97ac455279a7e92902ac1e9b5d98c5d4
parasite0 <37079401+parasite0@users.noreply.github.com>
2022-11-03 18:32:31 +0500

    Добавлен класс Message

--- ConsoleClient/Messenger/Program.cs
+++ ConsoleClient/Messenger/Program.cs
@@ -6,7 +6,8 @@ namespace Messenger
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Message msg = new Message();
+            Console.WriteLine(msg.ToString());
         }
     }
 }
